f6ee5272d7043c41e85a114d98975e8d8ab03e89
Raj kumar <186265760+Rajkr76@users.noreply.github.com>
2025-10-23 16:46:06 +0530

    fixing some issues in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://foodiehubproject.vercel.app/"],
     credentials:true
 }))
 app.use(express.json());
